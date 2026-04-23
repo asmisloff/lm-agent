f1be2ef1f6b0045fbf611e5d6f98104ecf88269d
asmisloff <a.smisloff@gmail.com>
2026-04-23 09:09:55 +0300

    Поиск файлов

--- src/main/java/ru/asmisloff/App.java
+++ src/main/java/ru/asmisloff/App.java
@@ -3,11 +3,9 @@ package ru.asmisloff;
 import com.openai.client.okhttp.OpenAIOkHttpClient;
 import com.openai.models.chat.completions.ChatCompletionCreateParams;
 
-import java.io.IOException;
-
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         var paramsBuilder = ChatCompletionCreateParams.builder()
             .model("deepseek/deepseek-v3.2")
             .addSystemMessage("""
@@ -15,7 +13,10 @@ public class App {
                                   Ты пишешь надежный, понятный и эффективный код. Комментарии и JavaDoc на русском языке, очень лаконично.
                                   Ты выводишь только код, комментарии и JavaDoc в markdown. Без дополнительных пояснений."""
             );
-        FileUtil.prompt().forEach(paramsBuilder::addUserMessage);
+        FileUtil.prompt().forEach(text -> {
+            paramsBuilder.addUserMessage(text);
+            System.out.println(text);
+        });
         var client = new OpenAIOkHttpClient.Builder()
             .baseUrl("https://routerai.ru/api/v1")
             .apiKey("sk-GpACFUI0S8-dKJOJ1x8XunGq0kG6kdek")
--- src/main/java/ru/asmisloff/FileUtil.java
+++ src/main/java/ru/asmisloff/FileUtil.java
@@ -1,28 +1,51 @@
 package ru.asmisloff;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 public class FileUtil {
 
-    public static Stream<String> prompt() {
+    public static List<String> prompt() {
         try (var reader = Files.newBufferedReader(Path.of("prompt.md"))) {
-            return reader.lines();
+            return reader.lines().toList();
         } catch (IOException ex) {
             System.out.println("Не удалось прочитать prompt.md из файловой системы");
         }
         try (var ins = FileUtil.class.getResourceAsStream("prompt.md");
              var reader = new BufferedReader(new InputStreamReader(Objects.requireNonNull(ins)))
         ) {
-            return reader.lines();
+            return reader.lines().toList();
         } catch (IOException | NullPointerException ex) {
             System.out.println("Не удалось прочитать промпт из classpath");
             throw new IllegalStateException(ex);
         }
+    }
+
+    public static List<Path> find(String pattern) {
+        try (var files = Files.walk(Path.of("."))) {
+            return files
+                .filter(path -> Files.isRegularFile(path) && containsIgnoreCase(path.toString(), pattern))
+                .toList();
+        } catch (IOException e) {
+            System.out.println("Не удалось получить список файлов");
+            return Collections.emptyList();
+        }
+    }
+
+    private static boolean containsIgnoreCase(@NotNull String str, @NotNull String substr) {
+        for (int i = 0; i <= str.length() - substr.length(); i++) {
+            if (str.regionMatches(true, i, substr, 0, substr.length())) {
+                return true;
+            }
+        }
+        return false;
     }
 }
